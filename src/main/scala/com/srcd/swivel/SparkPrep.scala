613d336e999d90dc94efaae8c17ea110ebaf44b6
Alexander Bezzubov <bzz@apache.org>
2017-05-02 20:21:04 +0200

    Initial data preparation logic

--- src/main/scala/com/srcd/swivel/SparkPrep.scala
+++ src/main/scala/com/srcd/swivel/SparkPrep.scala
@@ -1,41 +1,69 @@
 package com.srcd.swivel
 
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
 import scala.util.Properties
 
 
 object SparkPrep {
 
   def main(args: Array[String]): Unit = {
-    //input, output paths
+    //TODO(bzz): CLI args
+    //  --input <filename>
+    //  --output_dir <directory>
+    //  --shard_size <int>
+    //  --min_count <int>
+    //  --max_vocab <int>
+    //  --vocab <filename>
+    //  --window_size <int>
+
     val sparkMaster = Properties.envOrElse("MASTER", "local[*]")
-    val (sc, sqlContext) = getContexts(sparkMaster)
+    val (sc, spark) = getContext(sparkMaster)
 
     //create id->word, and word->id dicts
+    val dict = sc.textFile("tokens_10.tsv")
+      .flatMap(_.split("\t"))
+      .map(word => (word, 1))
+      .reduceByKey((a, b) => a + b)
 
-    // Optimisations
-    //-XX:+UseCompressedOops if <32Gb RAM
-    //http://fastutil.di.unimi.it
+    dict.collect() foreach println
+    // sort
+    // drop freq < FLAGS.min_count
+    // num_words = min(len(vocab), FLAGS.max_vocab)
+    // adjust vocab, so num_words % FLAGS.shard_size == 0
+    // keep top num_words
 
-    //broadcast id->word, and word->id dicts
-    //word -> ids
+    //Optimisations
+    // 4b pointers: -XX:+UseCompressedOops if <32Gb RAM
+    // tune DataStructures: http://fastutil.di.unimi.it
 
+    // broadcast id->word, and word->id dicts
 
+    //Build sharded co-ocurance matrix
+    // for each token => ( ( row,                col,                   val), (row, col, val), ...)
+    //                => ( ((row%i, row mod i), (col%i, col mod i),     val))
+    //                => ( row%i "+" col%j,     (row mod i, col mod i, val) )
+    //
+    //push sort to shuffle
+    // repartitionAndSortWithinPartitions(...)
+    // make sure it's partition by row%i,col%j
 
   }
 
 
-  def getContexts(sparkMaster: String): (SparkContext, SQLContext) = {
-    val conf = new SparkConf().
-      setMaster(sparkMaster).
-      setAppName(this.getClass.getSimpleName)
-
-    //conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+  def getContext(sparkMaster: String): (SparkContext, SparkSession) = {
     //conf.registerKryoClasses(Array(classOf[xxxx]))
 
-    val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
-    import sqlContext.implicits._
-    (sc, sqlContext)
+    val spark = SparkSession
+      .builder()
+      .appName(this.getClass.getSimpleName)
+      .master(sparkMaster)
+      //.config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .getOrCreate()
+
+    import spark.implicits._
+    (spark.sparkContext, spark)
   }
 
 }
